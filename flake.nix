94f13485f69ddb11428b1b94f632917b70480dea
Fitz35 <clement@lahoche.info>
2023-09-14 15:49:15 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,38 +1,64 @@
 
-
 {
   description = "flake support my_personnal_script nixos module";
 
-  
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-
-  
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.default =
-      # Notice the reference to nixpkgs here.
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "my_personnal_script";
-        src = self;
-
-        packages = with pkgs; [ 
-          rofi # menu
-          python3 # python
-          i3 # window manager
-          i3lock  # lock screen
-          kitty # terminal
-          feh # wallpaper
-        ];
-
-        buildPhase = ''
-          mkdir -p $out/bin;
-        '';
-        installPhase = ''
-          cp -r ./src/* $out/bin;
-          chmod +x $out/bin/my_script.sh;
-        '';
-      };
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs optional;
+      eachSystem = f: genAttrs
+        [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ]
+        (system: f nixpkgs.legacyPackages.${system});
+
+      my_personnal_script =
+        { pkgs, ... }:
+        pkgs.stdenv.mkDerivation {
+          name = "my_personnal_script_theme";
+          src = "${self}";
+
+          buildInputs = with pkgs; [ 
+            rofi # menu
+            python3 # python
+            i3 # window manager
+            i3lock  # lock screen
+            kitty # terminal
+            feh # wallpaper
+          ];
+
+
+          buildPhase = ''
+            mkdir -p $out/bin;
+          '';
+
+          installPhase = ''
+            cp -r ./src/* $out/bin;
+            chmod +x $out/bin/my_script.sh;
+          '';
+        };
+    in
+    {
+      nixosModules.default = { config, pkgs, ... }:
+        let
+          inherit (nixpkgs.lib) mkOption types mkIf;
+        in
+        {
+        
+
+          packages = eachSystem
+            (pkgs: {
+              default = my_personnal_script {
+                inherit pkgs;
+                # splash = "custom splash text";
+              };
+            });
+        };
+    }
 }
