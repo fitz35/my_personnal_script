ae28c0d202985725df7294077c64c9a055c8787a
Fitz35 <clement@lahoche.info>
2023-09-15 10:53:34 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,66 +1,30 @@
 {
   description = "flake support my_personnal_script nixos module";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs.lib) genAttrs optional;
-      eachSystem = f: genAttrs
-        [
-          "aarch64-darwin"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "x86_64-linux"
-        ]
-        (system: f nixpkgs.legacyPackages.${system});
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; stdenv.mkDerivation {
+      name = "my_personnal_script_theme";
+      src = self;
 
-      my_personnal_script =
-        { pkgs, splash ? "", customSplash ? splash != "", ... }:
-        pkgs.stdenv.mkDerivation {
-          name = "my_personnal_script_theme";
-          src = "${self}";
+      buildInputs = with pkgs; [
+        rofi     # menu
+        python3  # python
+        i3       # window manager
+        i3lock   # lock screen
+        kitty    # terminal
+        feh      # wallpaper
+      ];
 
-          buildInputs = with pkgs; [ 
-            rofi # menu
-            python3 # python
-            i3 # window manager
-            i3lock  # lock screen
-            kitty # terminal
-            feh # wallpaper
-          ];
+      buildPhase = ''
+        mkdir -p $out/bin;
+      '';
 
-
-          buildPhase = ''
-            mkdir -p $out/bin;
-          '';
-
-          installPhase = ''
-            cp -r ./src/* $out/bin;
-            chmod +x $out/bin/my_script.sh;
-          '';
-        };
-    in
-    {
-      nixosModules.default = { config, pkgs, ... }:
-        let
-          
-          inherit (nixpkgs.lib) mkOption types mkIf;
-        in
-        {
-          config = {
-            
-          };
-        };
-
-      packages = eachSystem
-        (pkgs: {
-          default = my_personnal_script {
-            inherit pkgs;
-            # splash = "custom splash text";
-          };
-        });
+      installPhase = ''
+        cp -r ./src/* $out/bin;
+        chmod +x $out/bin/my_script.sh;
+      '';
     };
+  };
 }
