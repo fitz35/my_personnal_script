c984e0533dc4391f551212b5d666c6112c96d60e
Fitz35 <clement@lahoche.info>
2023-09-22 08:50:35 +0200

    fix greenclip

--- src/script/greenclip.sh
+++ src/script/greenclip.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# get the window id of the active window to paste into
+WINDOW_ID=$(xdotool getactivewindow)
 sh "$DIR/../my_script.sh" rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
-
 sleep 0.5
 
 # Capture the selection
@@ -9,5 +11,7 @@ TEXT="$( xclip -o -selection clipboard )"
 
 # Only attempt to paste if there has been selection
 if [ "${TEXT}x" != "x" ]; then
-  xdotool type "$TEXT"
+    # xdotool type "$TEXT"
+    xdotool windowactivate --sync $WINDOW_ID
+    xdotool key --clearmodifiers Shift+Insert
 fi
