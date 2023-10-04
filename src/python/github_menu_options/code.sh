6865dc37ef9d839d2b997ae491e17063be1ee87c
Fitz35 <clement@lahoche.info>
2023-10-04 09:35:33 +0200

    check the nix develop command and fallback

--- src/python/github_menu_options/code.sh
+++ src/python/github_menu_options/code.sh
@@ -1,8 +1,19 @@
-cd $1
+#!/bin/bash
 
+# Change to the specified directory
+cd "$1"
+
+# Check if a flake.nix file exists in the current directory
 if test -e "flake.nix"; then
-    
+    # If flake.nix exists, use nix develop to set up the environment and open VSCode
     nix develop --command bash -c "code ."
+
+    # Check the exit status of the nix command
+    if [ $? -ne 0 ]; then
+        echo "nix command failed. Fallback to 'code .'"
+        code .
+    fi
 else
+    # If flake.nix doesn't exist, simply open VSCode in the current directory
     code .
 fi
