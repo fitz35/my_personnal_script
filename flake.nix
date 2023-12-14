0858002ec7354e001ebb478b98ca4f0c8365c742
Fitz35 <clement@lahoche.info>
2023-12-14 09:02:03 +0100

    fix nix version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "flake support my_personnal_script nixos module";
   
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     # Add other inputs as needed
   };
 
