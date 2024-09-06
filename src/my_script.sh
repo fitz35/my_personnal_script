7a2ee17bd817ebf869b939a4bb850dde32011431
Ort1e <lise@lahoche.com>
2024-09-06 11:07:00 +0200

    add a version check

--- src/my_script.sh
+++ src/my_script.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# if the first argument is -v or --version, print the version and exit
+
+if [ "$1" == "-v" ] || [ "$1" == "--version" ]; then
+    echo "my_script version 1.0.0"
+    exit 0
+fi
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 python3 ./my_script.py "$@"
