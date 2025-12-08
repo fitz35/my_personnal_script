dac658ff292c2793f2e7cce148e6d43cba066656
Ort1e <lise@lahoche.com>
2025-12-08 11:21:14 +0100

    update nix version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "flake support my_personnal_script nixos module";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     # Add other inputs as needed
   };
 
